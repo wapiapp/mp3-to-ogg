d94e00e93ea71764c7705340f603c7f32bc203d8
wapiapp <suportewapia@gmail.com>
2025-01-24 02:06:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,37 @@
 const express = require('express');
-const multer = require('multer');
 const ffmpeg = require('fluent-ffmpeg');
-const path = require('path');
 const ffmpegPath = require('ffmpeg-static');
-const fs = require('fs');
-
-// Verificar se a pasta uploads existe, se não, criar
-const uploadsDir = path.join(__dirname, 'uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
+const { Buffer } = require('buffer');
 
 ffmpeg.setFfmpegPath(ffmpegPath);
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadsDir); // Usando a pasta uploads criada
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-const upload = multer({ storage: storage });
 
 const app = express();
 const port = 3000;
 
-app.post('/convert', upload.single('audio'), (req, res) => {
-  console.log(req.file); // Log para depuração
+app.use(express.json({ limit: '10mb' })); // Limitar o tamanho do corpo da requisição
 
-  if (!req.file) {
-    return res.status(400).send('Nenhum arquivo enviado.');
+app.post('/convert', (req, res) => {
+  const { audioBase64 } = req.body;
+
+  if (!audioBase64) {
+    return res.status(400).send('Nenhum áudio enviado.');
   }
 
-  const inputPath = req.file.path;
-  const outputPath = path.join(uploadsDir, `${Date.now()}.ogg`);
+  const inputBuffer = Buffer.from(audioBase64, 'base64');
+  const outputBuffer = [];
 
-  console.log('Convertendo:', inputPath); // Log do arquivo de entrada
-
-  ffmpeg(inputPath)
-    .output(outputPath)
+  ffmpeg()
+    .input(inputBuffer)
+    .inputFormat('mp3') // Substitua pelo formato de entrada correto
     .audioCodec('libvorbis')
+    .toFormat('ogg')
+    .on('data', (chunk) => {
+      outputBuffer.push(chunk);
+    })
     .on('end', () => {
-      console.log('Conversão concluída:', outputPath); // Log do arquivo de saída
-      res.download(outputPath, (err) => {
-        if (err) {
-          console.log('Erro ao enviar o arquivo:', err);
-        }
-        // Remover arquivos após o envio
-        fs.unlinkSync(inputPath);
-        fs.unlinkSync(outputPath);
-      });
+      const finalBuffer = Buffer.concat(outputBuffer);
+      const outputBase64 = finalBuffer.toString('base64');
+      res.json({ audioBase64: outputBase64 });
     })
     .on('error', (err) => {
       console.error('Erro na conversão:', err);
